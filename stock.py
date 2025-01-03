18b4a5b20e75a67e25be15e985439563ff09b5be
pjtet <pjtetting@icloud.com>
2025-01-02 20:07:15 -0600

    Started learing with a video 2

--- stock.py
+++ stock.py
@@ -43,10 +43,60 @@ print("Accuracy: "+str(precision_score(test['Target'],preds)))
 
 combined = pd.concat([test['Target'],preds], axis=1)
 
-combined.plot()
+# combined.plot()
 
-def predict(train, test, predictor, model):
+def predict(train, test, predictors, model):
     model.fit(train[predictors], train['Target'])
     preds = model.predict(test[predictors])
     preds = pd.Series(preds, index=test.index, name="Predictions")
     combined = pd.concat([test['Target'],preds], axis=1)
+    return combined
+
+def backtest(data,model, predictors, start=2500, step=250):
+    all_prediction=[]
+
+    for i in range(start, data.shape[0], step):
+        train= data.iloc[0:i].copy()
+        test= data.iloc[i:(i+step)].copy()
+        predictions= predict(train, test, predictors,model)
+        all_prediction.append(predictions)
+
+    return pd.concat(all_prediction)
+
+predictions = backtest(sp500, model, predictors)
+
+predictions["Predictions"].value_counts()
+
+#precision_score(predictions['Target'].value_counts()/predictions.shape[0])
+
+horizons= [2,5,60,250,1000]
+new_predictors= []
+
+for horizons in horizons:
+    rolling_averages= sp500.rolling(horizons).mean()
+
+    ratio_column= f"Close_Ratio_{horizons}"
+    sp500[ratio_column]=sp500['Close']/rolling_averages['Close']
+
+    trend_column = f"Trend_{horizons}"
+    sp500[trend_column]=sp500.shift(1).rolling(horizons).sum()['Target']
+
+    new_predictors += [ratio_column, trend_column]
+
+sp500=sp500.dropna()
+
+model= RandomForestClassifier(n_estimators=500, min_samples_leaf=50,random_state=1)
+
+def predict(train, test, predictors, model):
+    model.fit(train[predictors], train['Target'])
+    preds = model.predict_proba(test[predictors])[:,1]
+    preds[preds>=.6]=1
+    preds[preds < .6] = 0
+    preds = pd.Series(preds, index=test.index, name="Predictions")
+    combined = pd.concat([test['Target'],preds], axis=1)
+    return combined
+
+predictions = backtest(sp500,model, new_predictors)
+predictions["Predictions"].value_counts()
+
+print(str(precision_score([predictions['Target']],predictions[predictions])))
